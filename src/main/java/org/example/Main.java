8759393d32ab838d00331458c6b1273bd2d3ca95
Dmitry Pytaylo <dpytaylo@gmail.com>
2024-12-01 17:46:14 +0200

    replace `double` to `int`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,22 +8,22 @@ public class Main {
 
         try {
             System.out.print("Enter a: ");
-            var a = scanner.nextDouble();
+            var a = scanner.nextInt();
 
             System.out.print("Enter b: ");
-            var b = scanner.nextDouble();
+            var b = scanner.nextInt();
 
             System.out.print("Enter c: ");
-            var c = scanner.nextDouble();
+            var c = scanner.nextInt();
 
             checkTriangle(a, b, c);
         } catch(Exception e) {
-            System.out.println("Треугольник не существует");
+            System.out.println("Невалидный ввод");
         }
     }
 
-    static void checkTriangle(double a, double b, double c) {
-        if (a < 0 || b < 0 || c < 0 || a + b <= c || a + c <= b || b + c <= a) {
+    static void checkTriangle(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0 || a <= c - b || a <= b - c || b <= a - c) {
             System.out.println("Треугольник не существует");
             return;
         }
